200bae3f934f3838ad117711a6ae003d94e6531c
iqtheengineer <82541131+iqtheengineer@users.noreply.github.com>
2022-03-09 14:59:38 -0500

    Create paycheck-calculator.py

---
+++ paycheck-calculator.py
@@ -0,0 +1,7 @@
+def calc_and_show_pay():
+    rate = input("How much is your hourly rate?: ")     
+    hours = input("How many hours did you work?: ")     
+    gross_pay = float(rate) * float(hours)  
+    print("Payroll Information:")           
+    print("Your pay is %f"%(gross_pay))     
+    return gross_pay    

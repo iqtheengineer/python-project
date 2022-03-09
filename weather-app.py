a97f4b7dc02b264a6a9d8702afba6bfdef2aec4a
iqtheengineer <82541131+iqtheengineer@users.noreply.github.com>
2022-03-09 14:53:31 -0500

    Create weather-app.py

---
+++ weather-app.py
@@ -0,0 +1,7 @@
+import time 
+WeatherData = {'Date': '2021-04-14', 'Temperature': 80, 'Units': 'Fahrenheit'}
+print('Date: ' + WeatherData['Date'])
+time.sleep(1)
+print('Temperature: ' + str(WeatherData['Temperature']))
+time.sleep(1)
+print('Units: ' + WeatherData['Units'])

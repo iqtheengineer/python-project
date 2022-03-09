025250ba46950d0462809cd7c8605aee94ae3485
iqtheengineer <82541131+iqtheengineer@users.noreply.github.com>
2022-03-09 14:51:55 -0500

    Create dictionaries.py

---
+++ dictionaries.py
@@ -0,0 +1,9 @@
+#Dictonaries
+dict1 = {1:10, 2:20}
+dict2 = {3:30, 4:40}
+dict3 = {5:50, 6:60}
+
+# Merge contents of dict1, dict2 & dict3
+dict4 = {**dict1 , **dict2, **dict3}
+#print('Dictionary 4 :')
+print(dict4)
